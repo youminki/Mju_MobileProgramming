243d06b9002fc2be86ef11914f8ab7cdda865449
youminki <dbalsrl7648@gmail.com>
2024-09-19 18:27:48 +0900

    fix : Problem1

--- app/src/main/java/com/example/mju_mobileprogramming/60192219_p1.kt
+++ app/src/main/java/com/example/mju_mobileprogramming/60192219_p1.kt
@@ -1,5 +1,14 @@
 package com.example.mju_mobileprogramming
 
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when{
+        age in 0..12 -> 15
+        age in 13..60 -> if(isMonday) 25 else 30
+        age in 61..100 -> 20
+        else -> -1
+    }
+}
+
 fun main() {
     val child = 5
     val adult = 28
@@ -8,13 +17,4 @@ fun main() {
     println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
     println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
     println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
-}
-fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    return when{
-        age in 0..12 -> 15
-        age in 13..60 -> if(isMonday) 25 else 30
-        age in 61..100 -> 20
-        else -> -1
-
-    }
 }
